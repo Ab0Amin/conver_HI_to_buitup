0b65ac9298d4faef7e2c510fa5dc444b12e8c262
ahmed amin <ahmed.amin.abd.elwadod@gmail.com>
2021-02-22 16:43:20 +0200

    Add project files.

---
+++ conver_HI_to_buitup/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace conver_HI_to_buitup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_convert = new System.Windows.Forms.Button();
+            this.checkBox_delete_converted_beam = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button_convert
+            // 
+            this.button_convert.Location = new System.Drawing.Point(21, 68);
+            this.button_convert.Name = "button_convert";
+            this.button_convert.Size = new System.Drawing.Size(75, 23);
+            this.button_convert.TabIndex = 0;
+            this.button_convert.Text = "convert";
+            this.button_convert.UseVisualStyleBackColor = true;
+            this.button_convert.Click += new System.EventHandler(this.button_convert_Click);
+            // 
+            // checkBox_delete_converted_beam
+            // 
+            this.checkBox_delete_converted_beam.AutoSize = true;
+            this.checkBox_delete_converted_beam.Location = new System.Drawing.Point(125, 74);
+            this.checkBox_delete_converted_beam.Name = "checkBox_delete_converted_beam";
+            this.checkBox_delete_converted_beam.Size = new System.Drawing.Size(91, 17);
+            this.checkBox_delete_converted_beam.TabIndex = 1;
+            this.checkBox_delete_converted_beam.Text = "delete original";
+            this.checkBox_delete_converted_beam.UseVisualStyleBackColor = true;
+            this.checkBox_delete_converted_beam.CheckedChanged += new System.EventHandler(this.checkBox_delete_converted_beam_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 179);
+            this.Controls.Add(this.checkBox_delete_converted_beam);
+            this.Controls.Add(this.button_convert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_convert;
+        private System.Windows.Forms.CheckBox checkBox_delete_converted_beam;
+    }
+}
+
---
+++ conver_HI_to_buitup/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+using T3D =Tekla.Structures.Geometry3d;
+using Tekla.Structures.Geometry3d;
+
+using Tekla.Structures.Model.UI;
+
+namespace conver_HI_to_buitup
+{
+    public partial class Form1 : Form
+    {
+        Model model = new Model();
+
+        public Beam create_plate(string Plate_width, string Plate_thik, string Plate_matrial, T3D.Point Plate_start_point, T3D.Point Plate_end_point)
+        {
+            Beam plate = new Beam();
+
+           plate.Profile.ProfileString = "PL" + Plate_width + "*" + Plate_thik;
+            plate.Material.MaterialString = Plate_matrial;
+            plate.Position.Rotation = Position.RotationEnum.TOP;
+            plate.Position.Depth = Position.DepthEnum.MIDDLE;
+            plate.Position.Plane = Position.PlaneEnum.MIDDLE;
+            plate.StartPoint = Plate_start_point;
+            plate.EndPoint = Plate_end_point;
+            plate.PartNumber.StartNumber = 101;
+            plate.PartNumber.Prefix = "W";
+            plate.AssemblyNumber.Prefix = "";
+            plate.AssemblyNumber.StartNumber = 9001;
+            plate.Name = "PLATE";
+
+            plate.Insert();
+            return plate;
+
+        }
+        public Beam create_plate(string Plate_width, string Plate_thik, string Plate_matrial, T3D.Point Plate_start_point, T3D.Point Plate_end_point , Position.RotationEnum rotationEnum )
+        {
+            Beam plate = new Beam();
+
+            plate.Profile.ProfileString = "PL" + Plate_width + "*" + Plate_thik;
+            plate.Material.MaterialString = Plate_matrial;
+            plate.Position.Rotation =  rotationEnum ;
+            plate.Position.Depth = Position.DepthEnum.MIDDLE;
+            plate.Position.Plane = Position.PlaneEnum.MIDDLE;
+            plate.StartPoint = Plate_start_point;
+            plate.EndPoint = Plate_end_point;
+            plate.PartNumber.StartNumber = 101;
+            plate.PartNumber.Prefix = "W";
+            plate.AssemblyNumber.Prefix = "";
+            plate.AssemblyNumber.StartNumber = 9001;
+            plate.Name = "PLATE";
+            plate.Insert();
+            return plate;
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_convert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // piccker
+
+                Picker picker_HI = new Picker();
+                Beam converted_part = picker_HI.PickObject(Picker.PickObjectEnum.PICK_ONE_PART) as Beam;
+
+                // parametr
+                Double beam_length = 0;
+                Double beam_web_height = 0;
+                Double beam_web_thik = 0;
+                Double beam_flg_width = 0;
+                Double beam_flg_thik = 0;
+                string beam_matrial = null;
+
+                //change coordinates
+                model.GetWorkPlaneHandler().SetCurrentTransformationPlane(new TransformationPlane(converted_part.GetCoordinateSystem()));
+                // get picked beam proprties
+                converted_part.GetReportProperty("LENGTH", ref beam_length);
+                converted_part.GetReportProperty("WEB_HEIGHT", ref beam_web_height);
+                converted_part.GetReportProperty("PROFILE.WEB_THICKNESS", ref beam_web_thik);
+                converted_part.GetReportProperty("WIDTH", ref beam_flg_width);
+                converted_part.GetReportProperty("PROFILE.FLANGE_THICKNESS", ref beam_flg_thik);
+                converted_part.GetReportProperty("MATERIAL", ref beam_matrial);
+
+
+                Solid solid_converted_bean = converted_part.GetSolid();
+                T3D.Point maxpoint = solid_converted_bean.MaximumPoint;
+                T3D.Point minpoint = solid_converted_bean.MinimumPoint;
+                // top plate points
+                T3D.Point top_plate_start_point = new T3D.Point(0, maxpoint.Y - beam_flg_thik / 2, 0);
+                T3D.Point top_plate_end_point = new T3D.Point(beam_length, maxpoint.Y - beam_flg_thik / 2, 0);
+
+                //bottom pate points
+                T3D.Point bottom_plate_start_point = new T3D.Point(0, minpoint.Y + beam_flg_thik / 2, 0);
+                T3D.Point bottom_plate_end_point = new T3D.Point(beam_length, minpoint.Y + beam_flg_thik / 2, 0);
+
+
+                //web plate points
+                T3D.Point web_plate_start_point = new T3D.Point(0, 0, 0);
+                T3D.Point web_plate_end_point = new T3D.Point(beam_length, 0, 0);
+
+
+
+
+
+                //plate insert
+                create_plate(beam_flg_width.ToString(), beam_flg_thik.ToString(), beam_matrial, top_plate_start_point, top_plate_end_point);
+                create_plate(beam_flg_width.ToString(), beam_flg_thik.ToString(), beam_matrial, bottom_plate_start_point, bottom_plate_end_point);
+                create_plate(beam_web_height.ToString(), beam_web_thik.ToString(), beam_matrial, web_plate_start_point, web_plate_end_point, Position.RotationEnum.FRONT);
+
+                //set coordinates to global
+                model.GetWorkPlaneHandler().SetCurrentTransformationPlane(new TransformationPlane());
+                // delete l beam
+                if (checkBox_delete_converted_beam.Checked == true)
+                {
+                    converted_part.Delete();
+                }
+              
+                model.CommitChanges();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            } 
+           
+            
+        }
+
+        private void checkBox_delete_converted_beam_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
